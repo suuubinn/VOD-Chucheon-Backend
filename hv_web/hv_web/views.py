e00862eac3f3c0383159d13f637c5520ff24f2a4
mino1998 <mhlee5894@gmail.com>
2023-11-17 09:16:27 +0900

    [init] #1 1117 로컬 테스트 백엔드 생성

---
+++ hv_web/hv_web/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+
+
+# Create your views here.
+def landing(request):
+    islogged = request.user.is_authenticated
+    if islogged:
+        return redirect('/main')
+    if request.method == "GET":
+        return render(request, 'landing.html')
+
+
+def main(request):
+    if request.method == "GET":
+        return render(request, 'main/main.html')
