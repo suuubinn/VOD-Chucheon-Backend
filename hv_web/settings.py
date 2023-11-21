0dfc5cfda1e053c8c9e02431913618894d02ca65
it-mnm <noble3769@gmail.com>
2023-11-21 09:18:16 +0900

    settings.py에 database 설정함

--- hv_web/settings.py
+++ hv_web/settings.py
@@ -76,13 +76,19 @@ WSGI_APPLICATION = 'hv_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "jintto",
+        "USER":"jintto",
+        "PASSWORD":"jintto1234",
+        "HOST":"vod-recommendation-db.cakeuxdlscyg.ap-northeast-2.rds.amazonaws.com",
+        "PORT":"3306"
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
