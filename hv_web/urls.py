6b819f3a5579ce56e0f8285e6f6f4c3b119b3668
sbkim <kjb7506@ajou.ac.kr>
2023-11-23 16:27:39 +0900

    project

--- hv_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.landing, name='landing'),
-    # path('', include('user.urls'))
-]
