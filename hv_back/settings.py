b4ef55d1bec261909823f3d445281f6e444b588a
Su-Min57 <hera2268@naver.com>
2023-12-01 17:02:56 +0900

    이게 진짜 다시시작

--- hv_back/settings.py
+++ hv_back/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from settings_secret import SECRET_KEY, DATABASES
+#from settings_secret import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
