bde960ba57c9f3304324c59a8aebc8a7f073a187
Su-Min57 <hera2268@naver.com>
2023-12-06 16:05:43 +0900

    *바꿨음, 도커파일들 포트번호 변경

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,5 @@ RUN pip install -r requirements.txt
 COPY . .
 
 #포트 개방/ 플라스크나 장고에서만 수행
-EXPOSE 80
-CMD ["python","manage.py","runserver","0.0.0.0:80"]
+EXPOSE 8000
+CMD ["python","manage.py","runserver","0.0.0.0:8000"]
--- hv_back/settings.py
+++ hv_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)6cv^nunnw2f864i*#i7g1la@%vshwx8k4z9^dt(fsy+_)e-b!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
